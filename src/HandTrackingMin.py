b0103f57342fdfbed6072b331f59f8978a392191
Eliud Kagema <eliud.k.maina@gmail.com>
2021-08-09 10:56:21 +0300

    setting up for loop for hand landmarks

--- src/HandTrackingMin.py
+++ src/HandTrackingMin.py
@@ -9,6 +9,10 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+# Previous time & Current time
+pTime = 0
+cTime =  0
+
 
 
 while True:
@@ -21,7 +25,15 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
 
     cv2.imshow("Image", img)
